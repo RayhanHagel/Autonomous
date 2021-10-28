cf93c12b61ec9d3eee17f4a1f102f56781d290de
RigeruSafa <43041149+RayhanHagel@users.noreply.github.com>
2021-10-28 19:02:03 +0700

    fixed #1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, json, base64, os
+import sys, json, time, os, threading
 from PyQt5 import QtWidgets
 from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox 
@@ -10,41 +10,17 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        loadUi("./ui/main.ui", self)
-        self.pushAccount.clicked.connect(lambda: self.goToScreen())
-        self.pushIndonesia.clicked.connect(lambda: self.Indonesia('809'))
-        self.pushEnglish.clicked.connect(lambda: self.Other('817'))
-        self.pushEconomy.clicked.connect(lambda: self.Other('885'))
-        self.pushPKWU.clicked.connect(lambda: self.Other('569'))
-        self.pushPhysics.clicked.connect(lambda: self.Other('833'))
-        self.pushMath1.clicked.connect(lambda: self.Other('970'))
-        self.pushMath1.clicked.connect(lambda: self.Other('571'))
-        self.pushPAI.clicked.connect(lambda: self.Other('1445'))
-        self.pushHistory.clicked.connect(lambda: self.Other('593'))
-        self.pushArt.clicked.connect(lambda: self.Other('593'))
-        
-    def goToScreen(self):
-        widget.setCurrentIndex(widget.currentIndex()+1)
-    
-    def Indonesia(self, id):
-        self.goToBrowser(id)
-        self.goToBrowser(id)
-        self.progress_done(id)
-
-    def Other(self, id):
-        self.goToBrowser(id)
-        self.progress_done(id)
-        
-    def goToBrowser(self, id):
+class browserThread(threading.Thread):
+    def __init__(self, type, id):
+        threading.Thread.__init__(self)
+        self.type = type
+        self.id = id
         with open('login.json', 'r') as f:
             data = json.load(f)
             self.username = data['Username']
-            password = base64.b64decode(data['Password'].encode("ascii")).decode("ascii")
-            self.password = base64.b64decode(password).decode("ascii")
-            
+            self.password = data['Password']
+        
+    def run(self):
         options = Options()
         options.add_argument("--headless")
         options.add_argument('--no-sandbox')
@@ -63,65 +39,119 @@ class MainWindow(QMainWindow):
         input_pass.send_keys(self.password)
         driver.find_element(By.ID, 'loginbtn').click()
         
-        driver.get(f'https://belajar.smansa-batam.sch.id/mod/attendance/view.php?id={id}')
-        try:
-            driver.find_element(By.PARTIAL_LINK_TEXT, "Submit attendance").click()
+        popUp = QMessageBox()
+        popUp.setIcon(QMessageBox.Information)
+        popUp.setWindowTitle("Task Done!")
+            
+        if self.type == 1:
+            popUp.setText(f"Submitted attendance on {id}")
+            driver.get(f'https://belajar.smansa-batam.sch.id/mod/attendance/view.php?id={self.id}')
             try:
-                driver.find_element(By.XPATH, '//*[@id="fgroup_id_statusarray"]/div[2]/fieldset/div/label[1]/span').click()
-                driver.find_element(By.ID, 'id_submitbutton').click()
+                driver.find_element(By.PARTIAL_LINK_TEXT, "Submit attendance").click()
+                try:
+                    while True:
+                        driver.find_element(By.XPATH, '//*[@id="fgroup_id_statusarray"]/div[2]/fieldset/div/label[1]/span').click()
+                        driver.find_element(By.ID, 'id_submitbutton').click()
+                except:
+                    pass
             except:
                 pass
-        except:
-            pass
-        
+            
+        elif self.type == 2:
+            popUp.setText(f"Done farming experience points on {id}")
+            driver.get(f'https://belajar.smansa-batam.sch.id/course/view.php?id={self.id}')
+            links, linkz = driver.find_elements(By.TAG_NAME, 'a'), []
+            for link in links:
+                link = link.get_attribute("href")
+                items = ['attendance', 'googlemeet', 'url', 'page', 'assign', 'forum']
+                for item in items:
+                    if link.find(item) != -1:
+                        linkz.append(link)
+            for x in linkz:
+                driver.get(x)
+                
         driver.quit()
-    
-    def progress_done(self, id):
-        popUp = QMessageBox()
-        popUp.setText(f"Done submitting attendance on {id}")
-        popUp.setIcon(QMessageBox.Information)
-        popUp.setWindowTitle("Progress")
         popUp.exec()
-    
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        loadUi("./ui/main.ui", self)
+        self.pushAttendance.clicked.connect(lambda: widget.setCurrentIndex(1))
+        self.pushAccount.clicked.connect(lambda: widget.setCurrentIndex(2))
+        self.pushFarming.clicked.connect(lambda: widget.setCurrentIndex(3))
+
+
+class AttendanceWindow(QMainWindow):
+    def __init__(self):
+        super(AttendanceWindow, self).__init__()
+        loadUi("./ui/attendance.ui", self)
+        self.pushBack.clicked.connect(lambda: widget.setCurrentIndex(0))
+        self.pushIndonesia.clicked.connect(lambda: browserThread(1, 809).start())
+        self.pushEnglish.clicked.connect(lambda: browserThread(1, 817).start())
+        self.pushEconomy.clicked.connect(lambda: browserThread(1, 885).start())
+        self.pushPKWU.clicked.connect(lambda: browserThread(1, 569).start())
+        self.pushPhysics.clicked.connect(lambda: browserThread(1, 833).start())
+        self.pushMath1.clicked.connect(lambda: browserThread(1, 970).start())
+        self.pushMath2.clicked.connect(lambda: browserThread(1, 571).start())
+        self.pushPAI.clicked.connect(lambda: browserThread(1, 1445).start())
+        self.pushHistory.clicked.connect(lambda: browserThread(1, 593).start())
+        self.pushArt.clicked.connect(lambda: browserThread(1, 598).start())
+
+
 class AccountWindow(QMainWindow):
     def __init__(self):
         super(AccountWindow, self).__init__()
         loadUi("./ui/account.ui", self)
         self.inputPassword.setEchoMode(QtWidgets.QLineEdit.Password)
         self.pushSave.clicked.connect(lambda: self.goToScreen())
-    
+
     def goToScreen(self):
         username = self.inputUsername.text()
         password = self.inputPassword.text()
-        self.savePassword(username, password)
-        widget.setCurrentIndex(widget.currentIndex()-1)
-    
-    def savePassword(self, username, password):
-        password = base64.b64encode(password.encode("ascii")).decode("ascii")
-        password = base64.b64encode(password.encode("ascii")).decode("ascii")
         if (username!='') and (password!=''):
             with open('login.json', 'w') as f:
                 json.dump({'Username': username, 'Password': password}, f, ensure_ascii=False, indent=4)
+        widget.setCurrentIndex(0)
+
+
+class FarmingWindow(QMainWindow):
+    def __init__(self):
+        super(FarmingWindow, self).__init__()
+        loadUi("./ui/farming.ui", self)
+        self.pushBack.clicked.connect(lambda: widget.setCurrentIndex(0))
+        self.pushIndonesia.clicked.connect(lambda: browserThread(2, 23).start())
+        self.pushEnglish.clicked.connect(lambda: browserThread(2, 26).start())
+        self.pushEconomy.clicked.connect(lambda: browserThread(2, 90).start())
+        self.pushPKWU.clicked.connect(lambda: browserThread(2, 77).start())
+        self.pushPhysics.clicked.connect(lambda: browserThread(2, 44).start())
+        self.pushMath1.clicked.connect(lambda: browserThread(2, 34).start())
+        self.pushMath2.clicked.connect(lambda: browserThread(2, 38).start())
+        self.pushPAI.clicked.connect(lambda: browserThread(2, 10).start())
+        self.pushHistory.clicked.connect(lambda: browserThread(2, 57).start())
+        self.pushArt.clicked.connect(lambda: browserThread(2, 84).start())
 
 
 if __name__ == '__main__':
     if os.path.isfile('login.json') == False:
         with open('login.json', 'w') as f:
             json.dump({'Username': 'None', 'Password': 'None'}, f, ensure_ascii=False, indent=4)
-    
+
     app = QApplication(sys.argv)
     widget = QtWidgets.QStackedWidget()
     widget.setWindowTitle("Attendance")
     widget.setMinimumSize(QSize(320, 240))
     widget.setMaximumSize(QSize(320, 240))
-    
+
     icon = QIcon()
     icon.addFile(u"./images/icon.ico", QSize(), QIcon.Normal, QIcon.Off)
     icon.addFile(u"./images/icon.ico", QSize(), QIcon.Selected, QIcon.On)
     widget.setWindowIcon(icon)
-    
+
     widget.addWidget(MainWindow())
+    widget.addWidget(AttendanceWindow())
     widget.addWidget(AccountWindow())
+    widget.addWidget(FarmingWindow())
     widget.show()
     sys.exit(app.exec_())
-    
